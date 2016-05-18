996f59122d5c20711fff82931732bd3127180664
Yanians <brian251989@gmail.com>
2016-05-19 03:03:28 +0800

    Add files via upload

---
+++ version-1.0/header.php
@@ -0,0 +1,9 @@
+<?php 
+echo "<html>\n";
+echo "<head>\n";
+echo "\t<title>includeTest.php</title>\n";
+echo "</head>\n";
+echo "<body>\n";
+echo "\t<!--BEGIN CONTENT-->\n";
+
+?>
---
+++ version-1.0/includeExample.php
@@ -0,0 +1,9 @@
+<?php  
+include 'header.phtml';
+?>
+<h1>Content Goes here</h1>
+
+<?php 
+include 'footer.html';
+
+?>
